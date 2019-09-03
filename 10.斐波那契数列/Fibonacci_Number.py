1070e90366cb3c3a81581b20046edc670a9381f5
michaelwng <michaelwangg@qq.com>
2019-09-03 22:10:38 +0800

    。。

---
+++ 10.斐波那契数列/Fibonacci_Number.py
@@ -0,0 +1,18 @@
+﻿class Solution(object):
+    def fib(self, N):
+        """
+        :type N: int
+        :rtype: int
+        """
+        if(N==0):
+            return 0
+        if(N==1 or N==2):
+            return 1
+        
+        f1 = 1
+        f2 = 1
+        for i in range(2,N):
+            tmp = f1+f2
+            f1 = f2
+            f2 = tmp
+        return f2
