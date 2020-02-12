2d3f2ebaf0dffa64b7216f84f3e82470db29a156
michaelwng <michaelwangg@qq.com>
2020-02-12 13:28:36 +0800

    增加一题

---
+++ 28. 对称的二叉树/dui-cheng-de-er-cha-shu-lcof.py
@@ -0,0 +1,17 @@
+﻿class Solution(object):
+	def isSymmetric(self, root):
+		"""
+		:type root: TreeNode
+		:rtype: bool
+		"""
+		if not root:
+			return True
+		def dfs(left,right):
+			if not (left or right):
+				return True
+			if not (left and right):
+				return False
+			if left.val!=right.val:
+				return False
+			return dfs(left.left,right.right) and dfs(left.right,right.left)
+		return dfs(root.left,root.right)
