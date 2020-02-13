94392bd8b40bd92ef5b922d656a15eba278d17ee
michaelwng <michaelwangg@qq.com>
2020-02-13 13:19:12 +0800

    .

---
+++ 6.从尾到头打印链表/cong-wei-dao-tou-da-yin-lian-biao-lcof.py
@@ -0,0 +1,11 @@
+﻿class Solution(object):
+	def reversePrint(self, head):
+		"""
+		:type head: ListNode
+		:rtype: List[int]
+		"""
+		res = []
+		while head:
+			res.insert(0,head.val)
+			head = head.next
+		return res
