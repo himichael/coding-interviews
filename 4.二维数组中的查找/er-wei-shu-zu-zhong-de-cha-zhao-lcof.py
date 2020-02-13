55ece3ffbcd91811586e3a93f90b41f823c7c38c
michaelwng <michaelwangg@qq.com>
2020-02-13 13:19:03 +0800

    增加一题

---
+++ 4.二维数组中的查找/er-wei-shu-zu-zhong-de-cha-zhao-lcof.py
@@ -0,0 +1,21 @@
+﻿class Solution(object):
+	def findNumberIn2DArray(self, matrix, target):
+		"""
+		:type matrix: List[List[int]]
+		:type target: int
+		:rtype: bool
+		"""
+		if not (matrix and matrix[0]):
+			return False
+		n = len(matrix)
+		m = len(matrix[0])
+		i = n-1
+		j = 0
+		while i>=0 and j<m:
+			if matrix[i][j]>target:
+				i -= 1
+			elif matrix[i][j]<target:
+				j += 1
+			else:
+				return True
+		return False
