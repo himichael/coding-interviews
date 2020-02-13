3817bb7519d4bfc3fbd0c5183f7355a68b247180
michaelwng <michaelwangg@qq.com>
2020-02-13 13:19:57 +0800

    1

---
+++ 10.II. 青蛙跳台阶问题/qing-wa-tiao-tai-jie-wen-ti-lcof.py
@@ -0,0 +1,15 @@
+﻿class Solution(object):
+	def numWays(self, n):
+		"""
+		:type n: int
+		:rtype: int
+		"""
+		if n==0:
+			return 1
+		f1 = 1
+		f2 = 1
+		for i in xrange(2,n+1):
+			tmp = f1+f2
+			f1 = f2
+			f2 = tmp
+		return f2%1000000007
