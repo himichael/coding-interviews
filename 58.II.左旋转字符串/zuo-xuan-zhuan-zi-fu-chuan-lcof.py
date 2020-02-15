89a7705eafd00c62d727577b674ce1f5f68a4a34
michaelwng <michaelwangg@qq.com>
2020-02-15 14:47:07 +0800

    左旋转字符串

---
+++ 58.II.左旋转字符串/zuo-xuan-zhuan-zi-fu-chuan-lcof.py
@@ -0,0 +1,17 @@
+﻿class Solution(object):
+	def reverseLeftWords(self, s, n):
+		"""
+		:type s: str
+		:type n: int
+		:rtype: str
+		"""
+		size = len(s)
+		return s[n:size]+s[0:n]
+		
+	def reverseLeftWords(self, s, n):
+		"""
+		:type s: str
+		:type n: int
+		:rtype: str
+		"""
+		return s[n:]+s[:n]		
