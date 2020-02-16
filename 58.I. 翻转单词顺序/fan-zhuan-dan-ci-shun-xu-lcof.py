b67088d48c60c54e8d9eb6912b930e35a6211f86
michaelwng <michaelwangg@qq.com>
2020-02-16 14:33:23 +0800

    增加内容

---
+++ 58.I. 翻转单词顺序/fan-zhuan-dan-ci-shun-xu-lcof.py
@@ -0,0 +1,7 @@
+﻿class Solution(object):
+	def reverseWords(self, s):
+		"""
+		:type s: str
+		:rtype: str
+		"""
+		return " ".join(s.split()[::-1])
--- 64. 求1+2+…+n/qiu-12n-lcof.py
+++
@@ -1,7 +0,0 @@
-﻿class Solution(object):
-	def sumNums(self, n):
-		"""
-		:type n: int
-		:rtype: int
-		"""
-		return n>0 and n+self.sumNums(n-1)
