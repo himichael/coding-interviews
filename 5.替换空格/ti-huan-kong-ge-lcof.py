06c5de1ebebf2c382e8fa696132c382980e3adee
michaelwng <michaelwangg@qq.com>
2020-02-19 20:44:27 +0800

    1

---
+++ 5.替换空格/ti-huan-kong-ge-lcof.py
@@ -0,0 +1,21 @@
+﻿class Solution(object):
+	def replaceSpace(self, s):
+		"""
+		:type s: str
+		:rtype: str
+		"""
+		if not s:
+			return s
+		arr = list(s)
+		for i in xrange(len(arr)):
+			if arr[i]==' ':
+				arr[i] = '%20'
+		return "".join(arr)
+		
+
+
+    def replaceSpace(self, s):
+        return s.replace(" ","%20")
+		
+		
+		
