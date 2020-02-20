b1f37b4a571e63f679ec46226e3a51327a6b3141
yang.wang09 <yang.wang09@ucarinc.com>
2020-02-20 14:41:29 +0800

    增加内容

---
+++ 42.连续子数组的最大和/lian-xu-zi-shu-zu-de-zui-da-he-lcof.py
@@ -0,0 +1,17 @@
+﻿class Solution(object):
+	def maxSubArray(self, nums):
+		"""
+		:type nums: List[int]
+		:rtype: int
+		"""	
+		if not nums:
+			return 0
+		res = nums[0]
+		sum = 0
+		for i in nums:
+			if sum>=0:
+				sum += i
+			else:
+				sum = i
+			res = max(res,sum)
+		return res
