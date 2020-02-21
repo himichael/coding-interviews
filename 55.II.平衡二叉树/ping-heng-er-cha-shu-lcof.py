2c9df4255ce82c3b75408b54441d482c53e51e66
yang.wang09 <yang.wang09@ucarinc.com>
2020-02-21 10:31:53 +0800

    平衡二叉树

---
+++ 55.II.平衡二叉树/ping-heng-er-cha-shu-lcof.py
@@ -0,0 +1,18 @@
+﻿class Solution(object):
+	def isBalanced(self, root):
+		"""
+		:type root: TreeNode
+		:rtype: bool
+		"""
+		if not root:
+			return True
+		def dfs(root):
+			if not root:
+				return 0
+			left = dfs(root.left)
+			right = dfs(root.right)
+			return max(left,right)+1
+		r = abs(dfs(root.left)-dfs(root.right))<=1
+		left = self.isBalanced(root.left)
+		right = self.isBalanced(root.right)
+		return r and left and right
