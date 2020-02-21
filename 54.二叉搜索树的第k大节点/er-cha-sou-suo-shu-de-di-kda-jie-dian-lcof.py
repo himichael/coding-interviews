76f0e973b5f5e9d16dc57a2865ba4da9eaf58154
yang.wang09 <yang.wang09@ucarinc.com>
2020-02-21 10:28:42 +0800

    二叉搜索树的第k大节点

---
+++ 54.二叉搜索树的第k大节点/er-cha-sou-suo-shu-de-di-kda-jie-dian-lcof.py
@@ -0,0 +1,17 @@
+﻿class Solution(object):
+	def kthLargest(self, root, k):
+		if not root:
+			return 0
+		self.res = 0
+		self.index = 0
+		def dfs(root):
+			if not root:
+				return
+			dfs(root.right)
+			self.index += 1
+			if self.index==k:
+				self.res = root.val
+				return
+			dfs(root.left)
+		dfs(root)
+		return self.res
