2a645a91b66abb4b1d9b742de1e8132dea747a71
yang.wang09 <yang.wang09@ucarinc.com>
2020-02-21 10:27:08 +0800

    从上到下打印二叉树 III

---
+++ 32.III.从上到下打印二叉树/cong-shang-dao-xia-da-yin-er-cha-shu-iii-lcof.py
@@ -0,0 +1,20 @@
+﻿class Solution(object):
+	def levelOrder(self, root):
+		"""
+		:type root: TreeNode
+		:rtype: List[List[int]]
+		"""
+		res = []
+		def dfs(index,root):
+			if not root:
+				return
+			if len(res)<index:
+				res.append([])
+			if index%2==0:
+				res[index-1].insert(0,root.val)
+			else:
+				res[index-1].append(root.val)
+			dfs(index+1,root.left)
+			dfs(index+1,root.right)
+		dfs(1,root)
+		return res
