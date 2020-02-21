4490930f50736cd297cbf9fa8fe25c206b3e8ac1
yang.wang09 <yang.wang09@ucarinc.com>
2020-02-21 10:20:09 +0800

    面试题32 - I. 从上到下打印二叉树

---
+++ 32.I.从上到下打印二叉树/cong-shang-dao-xia-da-yin-er-cha-shu-lcof.py
@@ -0,0 +1,20 @@
+﻿class Solution(object):
+	def levelOrder(self, root):
+		"""
+		:type root: TreeNode
+		:rtype: List[int]
+		"""
+		if not root:
+			return []
+		queue = [root]
+		res = []
+		while queue:
+			size = len(queue)
+			for _ in xrange(size):
+				tmp = queue.pop(0)
+				res.append(tmp.val)
+				if tmp.left:
+					queue.append(tmp.left)
+				if tmp.right:
+					queue.append(tmp.right)
+		return res
