39029a1f46cea878bb2aa840e6d97d731482a706
michaelwng <michaelwangg@qq.com>
2020-02-28 21:42:12 +0800

    数值的整数次方

---
+++ 16.数值的整数次方/shu-zhi-de-zheng-shu-ci-fang-lcof.py
@@ -0,0 +1,12 @@
+﻿class Solution(object):
+	def myPow(self, x, n):
+		if n==0:
+			return 1.0
+		if n==1:
+			return x
+		if n<0:
+			return 1/self.myPow(x,n*-1)
+		if n%2==1:
+			return x * self.myPow(x,n-1)
+		else:
+			return self.myPow(x*x,n/2)
