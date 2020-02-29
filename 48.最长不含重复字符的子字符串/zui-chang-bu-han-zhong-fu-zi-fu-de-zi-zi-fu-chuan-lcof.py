9071bca79db420408d47ad19428f6269eeeb2512
michaelwng <michaelwangg@qq.com>
2020-02-29 15:14:11 +0800

    面试题48. 最长不含重复字符的子字符串

---
+++ 48.最长不含重复字符的子字符串/zui-chang-bu-han-zhong-fu-zi-fu-de-zi-zi-fu-chuan-lcof.py
@@ -0,0 +1,17 @@
+﻿class Solution(object):
+	def lengthOfLongestSubstring(self, s):
+		"""
+		:type s: str
+		:rtype: int
+		"""
+		if not s:
+			return 0
+		d = dict()
+		left = 0
+		res = 0
+		for i in xrange(len(s)):
+			if s[i] in d:
+				left = max(left,d[s[i]]+1)
+			d[s[i]] = i
+			res = max(res,i-left+1)
+		return res
