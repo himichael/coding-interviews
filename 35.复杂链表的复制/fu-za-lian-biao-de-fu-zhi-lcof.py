eefb5330ca40b65acda45762b07370f6673e1d10
michaelwng <michaelwangg@qq.com>
2020-02-29 18:20:28 +0800

    35题增加新实现

--- 35.复杂链表的复制/fu-za-lian-biao-de-fu-zhi-lcof.py
+++ 35.复杂链表的复制/fu-za-lian-biao-de-fu-zhi-lcof.py
@@ -22,3 +22,21 @@
 			p3.next = cur.next
 			p3 = p3.next
 		return p.next
+		
+		
+		
+	def copyRandomList(self, head):
+		"""
+		:type head: Node
+		:rtype: Node
+		"""
+		p1,p2 = head,head
+		d = dict()
+		while p1:
+			d[p1] = Node(p1.val,None,None)
+			p1 = p1.next
+		while p2:
+			d[p2].next = d[p2.next] if p2.next else None
+			d[p2].random = d[p2.random] if p2.random else None
+			p2 = p2.next
+		return d[head] if head else None		
