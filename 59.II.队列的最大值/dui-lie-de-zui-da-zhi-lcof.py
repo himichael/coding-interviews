7beb16cfbc833478d07bfeb2d332e8318d065842
michaelwng <michaelwangg@qq.com>
2020-03-07 15:18:47 +0800

    增加一题，队列的最大值

---
+++ 59.II.队列的最大值/dui-lie-de-zui-da-zhi-lcof.py
@@ -0,0 +1,43 @@
+﻿class MaxQueue(object):
+	def __init__(self):
+		import collections
+		self.queue = collections.deque()
+		self.inner = collections.deque()
+
+	def max_value(self):
+		"""
+		:rtype: int
+		"""
+		if not self.inner:
+			return -1
+		return self.inner[0]
+
+	def push_back(self, value):
+		"""
+		:type value: int
+		:rtype: None
+		"""
+		self.queue.append(value)
+		while self.inner and self.inner[-1]<value:
+			self.inner.pop()
+		self.inner.append(value)
+		
+
+	def pop_front(self):
+		"""
+		:rtype: int
+		"""
+		if not self.queue:
+			return -1
+		res = self.queue.popleft()
+		if res==self.inner[0]:
+			self.inner.popleft()
+		return res
+			
+
+
+# Your MaxQueue object will be instantiated and called as such:
+# obj = MaxQueue()
+# param_1 = obj.max_value()
+# obj.push_back(value)
+# param_3 = obj.pop_front()
