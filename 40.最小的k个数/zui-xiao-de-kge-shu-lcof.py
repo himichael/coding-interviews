e518baab94476d074117697b1c27679b86db95ac
michael <michaelwangg@qq.com>
2020-03-20 10:57:59 +0800

    增加一题，最小的k个数

---
+++ 40.最小的k个数/zui-xiao-de-kge-shu-lcof.py
@@ -0,0 +1,16 @@
+﻿class Solution(object):
+	def getLeastNumbers(self, arr, k):
+		if not arr or k<=0:
+			return []
+		if len(arr)<=k:
+			return arr
+		import heapq
+		queue = [-i for i in arr[:k]]
+		heapq.heapify(queue)
+		for i in xrange(k,len(arr)):
+			if -queue[0]>arr[i]:
+				heapq.heappop(queue)
+				heapq.heappush(queue,-arr[i])
+		for i in xrange(k):
+			queue[i] *= -1
+		return queue
