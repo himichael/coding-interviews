2bd43f018730725248b87384f896e9c43ac1ab71
yang.wang09 <yang.wang09@ucarinc.com>
2020-04-08 13:18:37 +0800

    第13题，机器人的运动范围 增加广度优先实现

--- 13.机器人的运动范围/ji-qi-ren-de-yun-dong-fan-wei-lcof.py
+++ 13.机器人的运动范围/ji-qi-ren-de-yun-dong-fan-wei-lcof.py
@@ -1,4 +1,5 @@
 ﻿class Solution(object):
+	# DFS实现 
 	def movingCount(self, m, n, k):
 		"""
 		:type m: int
@@ -25,4 +26,30 @@
 				y = j + dy[step]
 				dfs(x,y)
 		dfs(0,0)
+		return len(cache)
+		
+		
+		
+	# 广度优先实现
+	def movingCount(self, m, n, k):
+		dx = [0,1,0,-1]
+		dy = [1,0,-1,0]
+		cache = set()
+		def sum(i,j):
+			res = 0
+			while i>0 or j>0:
+				res += i%10 + j%10
+				i //= 10
+				j //= 10
+			return res
+		queue = [(0,0)]
+		while queue:
+			i,j = queue.pop(0)
+			if 0>i or i==m or 0>j or j==n or sum(i,j)>k or (i,j) in cache:
+				continue
+			cache.add((i,j))
+			for step in xrange(4):
+				x = i + dx[step]
+				y = j + dy[step]
+				queue.append((x,y))
 		return len(cache)
