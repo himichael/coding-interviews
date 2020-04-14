711ef94879869bbe585c8d3fd56242c790a05359
yang.wang09 <yang.wang09@ucarinc.com>
2020-04-14 14:34:42 +0800

    第40题，最小的k个数，增加一种实现方式

--- 40.最小的k个数/zui-xiao-de-kge-shu-lcof.py
+++ 40.最小的k个数/zui-xiao-de-kge-shu-lcof.py
@@ -15,3 +15,33 @@
 		for i in xrange(k):
 			queue[i] *= -1
 		return queue
+		
+	# 快速选择
+	def getLeastNumbers(self, arr, k):
+		if not arr or k<=0:
+			return []
+		if len(arr)<=k:
+			return arr
+		def partitioin(begin,end):
+			povit = arr[end]
+			i = begin
+			for j in xrange(begin,end):
+				if arr[j]<povit:
+					arr[i],arr[j] = arr[j],arr[i]
+					i += 1
+			arr[i],arr[end] = arr[end],arr[i]
+			return i
+		def quick_search(begin,end,k):
+			if begin>=end:
+				return
+			pos = partitioin(begin,end)
+			if pos>k:
+				quick_search(begin,pos-1,k)
+			elif pos<k:
+				quick_search(pos+1,end,k)
+			else:
+				return
+		quick_search(0,len(arr)-1,k)
+		return arr[:k]
+		
+		
