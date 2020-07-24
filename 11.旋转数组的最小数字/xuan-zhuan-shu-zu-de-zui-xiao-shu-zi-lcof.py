25610a6ec645a98006941ea2ad9fdb2bc1318c17
michaelwng <michaelwangg@qq.com>
2020-07-24 12:28:31 +0800

    增加第11题

---
+++ 11.旋转数组的最小数字/xuan-zhuan-shu-zu-de-zui-xiao-shu-zi-lcof.py
@@ -0,0 +1,13 @@
+﻿class Solution(object):
+    def minArray(self, numbers):
+        begin = 0
+        end = len(numbers)-1
+        while begin<=end:
+            mid = begin+(end-begin)//2
+            if numbers[mid]>numbers[end]:
+                begin = mid+1
+            elif numbers[mid]<numbers[end]:
+                end = mid
+            else:
+                end -= 1
+        return numbers[begin]
