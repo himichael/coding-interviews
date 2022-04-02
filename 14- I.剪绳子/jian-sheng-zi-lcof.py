701dbe0fe1f6bb9e2daae47b77747af822999542
超级呆子 <michaelwangg@qq.com>
2022-04-02 13:40:04 +0800

    增加两道题，剪绳子1和2

---
+++ 14- I.剪绳子/jian-sheng-zi-lcof.py
@@ -0,0 +1,11 @@
+﻿class Solution(object):
+    def cuttingRope(self, n):
+        if n <= 1:
+            return 0 if n == 0 else 1
+        dp = [0] * (n + 1)
+        for i in range(2, n + 1):
+            tmp = 0
+            for j in range(1, i):
+                tmp = max(tmp, j * (i - j), j * dp[i - j])
+            dp[i] = tmp
+        return dp[-1]
---
+++ 14- II.剪绳子 II/jian-sheng-zi-ii-lcof.py
@@ -0,0 +1,13 @@
+﻿class Solution(object):
+    def cuttingRope(self, n):
+        if n <= 1:
+            return 0 if n == 0 else 1
+        val = 10 ** 9 + 7
+        dp = [0] * (n + 1)
+        for i in range(2, n + 1):
+            tmp = 0
+            for j in range(1, i):
+                tmp = max(tmp, j * (i - j), j * dp[i - j])
+            dp[i] = tmp
+        return dp[-1] % val
+
